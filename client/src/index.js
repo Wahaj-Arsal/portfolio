109fb2d7dcc6397c0a11e9596855222d08f55c0a
Wahaj-Arsal <83618649+Wahaj-Arsal@users.noreply.github.com>
2023-03-22 20:43:35 +0000

    adding back end

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
