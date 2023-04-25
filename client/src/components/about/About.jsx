83af7b9abe18b6ac40c24a22edab1e3a815718e2
Wahaj-Arsal <83618649+Wahaj-Arsal@users.noreply.github.com>
2023-04-26 00:11:01 +0100

    responsive work done, footer remaining, logo updated

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -13,33 +13,39 @@ export default function About() {
         <h5 className="about__name">Wahaj Arsal</h5>
         <h2 className="about__title">About Me</h2>
         <div className="about__information">
-          <img className="about__image" src={profilePicture} alt="profile" />
-          <p className="about__text">
-            A Full Stack Web Developer with a background in Business Analysis.
-            I'm a detail-oriented problem solver with expertise in HTML, CSS,
-            JavaScript, and React.
-            <br />
-            <br />
-            I'm passionate about building user-friendly websites and
-            applications that deliver tangible results.
-          </p>
-          <div className="skills-container">
-            <h5 className="skills">
-              Here are some of the technologies I have worked with
-            </h5>
-            <ul className="about__tech">
-              <li className="about__skill">HTML/HTML5</li>
-              <li className="about__skill">CSS/CSS3</li>
-              <li className="about__skill">SASS</li>
-              <li className="about__skill">JavaScript</li>
-              <li className="about__skill">React.js</li>
-              <li className="about__skill">Node.js</li>
-              <li className="about__skill">Express.js</li>
-              <li className="about__skill">MySQL</li>
-              <li className="about__skill">Stripe</li>
-              <li className="about__skill">EmailJS</li>
-              <li className="about__skill">MapBox</li>
-            </ul>
+          <div className="about__picture">
+            <img className="about__image" src={profilePicture} alt="profile" />
+          </div>
+          <div className="about__details">
+            <p className="about__text">
+              A Full Stack Web Developer with a background in Business Analysis.
+              I'm a detail-oriented problem solver with expertise in HTML, CSS,
+              JavaScript, and React.
+              <br />
+              I'm passionate about building user-friendly websites and
+              applications that deliver tangible results.
+              <br />
+            </p>
+            <div className="skills-container">
+              <h5 className="skills">
+                Here are some of the technologies I have worked with
+              </h5>
+              <ul className="about__tech">
+                <li className="about__skill">HTML/HTML5</li>
+                <li className="about__skill">CSS/CSS3</li>
+                <li className="about__skill">SCSS</li>
+                <li className="about__skill">JavaScript</li>
+                <li className="about__skill">React.js</li>
+                <li className="about__skill">Angular</li>
+                <li className="about__skill">Node.js</li>
+                <li className="about__skill">Express.js</li>
+                <li className="about__skill">MySQL</li>
+                <li className="about__skill">PHP</li>
+                <li className="about__skill">Stripe</li>
+                <li className="about__skill">EmailJS</li>
+                <li className="about__skill">MapBox</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
